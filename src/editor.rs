a2acc6cb66db90ff59e4243860804545d034037e
Subham Singh <geekysubham@gmail.com>
2023-09-14 02:15:46 +0530

    structure code into multiple files

---
+++ src/editor.rs
@@ -0,0 +1,39 @@
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => match key {
+                    Key::Char(c) => {
+                        if c.is_control() {
+                            println!("{:?}\r", c as u8);
+                        } else {
+                            println!("{:?} ({})\r", c as u8, c);
+                        }
+                    }
+                    Key::Ctrl('q') => break,
+
+                    // _ is called for every case that has not already been handled.
+                    // It is a catch-all case. It is also called the wildcard case. Default option.
+                    _ => println!("{:?}\r", key),
+                },
+                Err(err) => die(err),
+            }
+        }
+    }
+    pub fn default() -> Self {
+        Editor {}
+    }
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,7 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
+mod editor;
+use editor::Editor;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?} \r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-
-                // _ is called for every case that has not already been handled.
-                // It is a catch-all case. It is also called the wildcard case. Default option.
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
+    let editor = Editor::default();
+    editor.run();
 }
