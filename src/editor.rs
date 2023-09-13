1588e5538e6dfde9417111b3e2f9b96fc72325ef
Subham Singh <geekysubham@gmail.com>
2023-09-14 02:55:54 +0530

    seprate read from evaluation

--- src/editor.rs
+++ src/editor.rs
@@ -3,37 +3,48 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
 
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?}\r", c as u8);
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    }
-                    Key::Ctrl('q') => break,
-
-                    // _ is called for every case that has not already been handled.
-                    // It is a catch-all case. It is also called the wildcard case. Default option.
-                    _ => println!("{:?}\r", key),
-                },
-                Err(err) => die(err),
+        loop {
+            if let Err(error) = self.process_keypress(){
+                die(error);   
+            }
+            if self.should_quit{
+                break;
             }
         }
     }
+
     pub fn default() -> Self {
-        Editor {}
+        Self { should_quit: false }
     }
+
+    // () means nothing. returns Ok, Err
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?; // If there’s an error, return it, if not, unwrap the value and continue
+        match pressed_key {
+            Key::Ctrl('q') => self.should_quit = true,
+            _ => (), // Everything is OK
+        }
+        Ok(())
+    }
+
 }
 
 fn die(e: std::io::Error) {
     panic!("{}", e);
+}
+
+fn read_key() -> Result<Key, std::io::Error> {
+    loop{
+        if let Some(key) = io::stdin().lock().keys().next(){
+            return key;
+        }
+    }
 }
