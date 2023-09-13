955bb25e0d201718895a09d8afadcb7109f4dedf
Subham Singh <geekysubham@gmail.com>
2023-09-14 00:50:39 +0530

    read keys instead of bytes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
 
 fn die(e: std::io::Error) {
     panic!("{}", e);
@@ -13,24 +10,22 @@ fn die(e: std::io::Error) {
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => {
+                    if c.is_control() {
+                        println!("{:?} \r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
                 }
+                Key::Ctrl('q') => break,
 
-                // println!("{:#b}\r", b); // binary representation of b
-
-                if b == to_ctrl_byte('q') {
-                    println!("Exiting.");
-                    break;
-                }
-            }
+                // _ is called for every case that has not already been handled.
+                // It is a catch-all case. It is also called the wildcard case. Default option.
+                _ => println!("{:?}\r", key),
+            },
             Err(err) => die(err),
         }
     }
